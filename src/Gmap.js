45cc506acef0a6a09ed34ef548b7c61851d64f71
Praveen Bennur <praveen.bennur.srsti@gmail.com>
2018-11-28 20:10:03 +0530

    Implemented feature to zoom and center. When clicked on marker and when clicked on map zoom and center to default values

--- src/Gmap.js
+++ src/Gmap.js
@@ -1,17 +1,57 @@
 import React, { Component } from 'react';
 
 class Gmap extends Component {
+
+  // handleInfoWindows = (map) => {
+  //   let infowindow = new window.google.maps.InfoWindow();
+  //   window.google.maps.event.addListener(marker, 'click', function(){
+  //     infowindow.open(map, marker);
+  //   })
+  // }
+
+  defaultMapParams = (mapToMarkOn) => {
+    let bangalore = {lat: 12.9716, lng: 77.5946};
+    //when clicked on the map zoom out and center to default values
+    //and close infowindow
+    window.google.maps.event.addListener(mapToMarkOn, 'click', function() {
+      mapToMarkOn.setZoom(12)
+      mapToMarkOn.setCenter(bangalore)
+    })
+  }
+
   positionMarkers = (mapToMarkOn) => {
-    console.log("Positionsmarkers " + this.props.venues + "<=")
+
+    // console.log("Positionsmarkers " + this.props.venues + "<=")
     this.props.setMarkersState(
       this.props.venues.map( currentPos => {
-
         let marker = new window.google.maps.Marker({
           position: {
             lat: currentPos.venue.location.lat,
             lng: currentPos.venue.location.lng },
-          map: mapToMarkOn
+          map: mapToMarkOn,
+          title: currentPos.venue.name
         })
+        
+        // let contentString = `${currentPos.venue.name}`
+        // let infowindow = new window.google.maps.InfoWindow({
+        //   content: contentString
+        // })
+
+        // marker.addListener('click', function() {
+        //   infowindow.open(mapToMarkOn, marker);
+        // })
+
+        //when clicked on a marker zoom in and make its position
+        //as maps center
+        window.google.maps.event.addListener(marker, 'click', function() {
+          mapToMarkOn.setZoom(16)
+          mapToMarkOn.setCenter(marker.getPosition())
+        })
+
+        //when clicked on the map set the zoom and map center 
+        //to default values
+        this.defaultMapParams(mapToMarkOn)
+
         return marker
       })
     )
@@ -20,18 +60,15 @@ class Gmap extends Component {
   initGmap = () => {
     let bangalore = {lat: 12.9716, lng: 77.5946};
     let map = new window.google.maps.Map(
-    document.getElementById('map'), {zoom: 14, center: bangalore});
+    document.getElementById('map'), {zoom: 12, center: bangalore});
 
-    console.log("initv" + this.props.venues)
-    // The marker, positioned at bangalore
+    // The markers positioned at bangalore
     this.positionMarkers(map);
   }
 
   createScriptTag = () => {
     window.initGmap = this.initGmap;
     window.API_KEY = process.env.REACT_APP_GMAP_API_KEY;
-    console.log(window.API_KEY)
-    console.log(process.env.REACT_APP_GMAP_API_KEY);
 
     //create a promise to check if the variable 'google' is available before
     //map initialisation.
